19fd01fc5fbf9c1c0f584b7e1832484097517904
Semiz Software <semisft@gmail.com>
2020-05-29 14:16:49 +0300

    Initial Import

---
+++ src/main/java/com/semiz/giteducation/Main.java
@@ -0,0 +1,14 @@
+package com.semiz.giteducation;
+
+public class Main {
+
+	static int[] data = { 1, 2, 4, 8 };
+
+	public static void main(String[] args) {
+		for (int i : data) {
+			System.out.println(i);
+		}
+
+	}
+
+}
