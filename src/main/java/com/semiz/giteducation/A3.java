c3da6917165f1fc1f114de1f3bc23bb3cad1154f
Semiz Software <semisft@gmail.com>
2020-06-12 12:24:47 +0300

    Feat-2

--- src/main/java/com/semiz/giteducation/A3.java
+++ src/main/java/com/semiz/giteducation/A3.java
@@ -1,8 +1,14 @@
 package com.semiz.giteducation;
 
+import java.util.List;
+
 public class A3 {
 
 	public static void main1(String[] args) {
 		System.out.println("A3 step 1");
+	}
+	
+	public Integer getMax(List<Integer> vals) {
+		return vals.stream().mapToInt(v -> v.intValue()).max().getAsInt();
 	}
 }
