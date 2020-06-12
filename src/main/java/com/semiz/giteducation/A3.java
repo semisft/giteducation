2a39005b7f32d958c428e582f761dfb0f87fc5e7
Semiz Software <semisft@gmail.com>
2020-06-12 12:14:10 +0300

    A3 change 1

--- src/main/java/com/semiz/giteducation/A3.java
+++ src/main/java/com/semiz/giteducation/A3.java
@@ -2,7 +2,7 @@ package com.semiz.giteducation;
 
 public class A3 {
 
-	public static void main(String[] args) {
+	public static void main1(String[] args) {
 		System.out.println("A3 step 1");
 	}
 }
